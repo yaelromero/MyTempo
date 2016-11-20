b90ad77bfd48b7a4e0d6a0dc595ad5b9b75521ff
Yael Romero <romeroyael@gmail.com>
2016-11-20 13:47:39 -0500

    Assets placed, basic navigation done, buttons created

---
+++ Assets/Scripts/Nav.js
@@ -0,0 +1,37 @@
+﻿#pragma strict
+import UnityEngine.SceneManagement;
+import UnityEngine.EventSystems;
+import UnityEditor;
+
+var level = 0;
+
+function NextPage() {
+	Debug.Log("clicked");
+	SceneManager.LoadScene(++level);
+}
+
+function PreviousPage() {
+	SceneManager.LoadScene(--level);
+}
+
+function Exit() {
+	Debug.Log("quit");
+	if(EditorApplication.isPlaying) {
+		EditorApplication.isPlaying = false;
+	}
+	else {
+		Application.Quit();
+	}
+}
+
+function Start() {
+	Debug.Log("started");
+}
+
+function Update() {
+
+}
+
+function OnMouseDown() {
+	Debug.Log("clicked2");
+}
